f4db5e117c2e6e822ad66787bb2b776cbefdb229
Agnieszka.Gabka <agn.gabka@gmail.com>
2022-10-09 20:06:12 +0200

    numpy lab 2 part II

--- numpy_lab_2.py
+++ numpy_lab_2.py
@@ -46,18 +46,26 @@ arr = np.arange(50).reshape(10, 5)
 arr
 
 # how much data should be "test-data' - here 20%
-split_level = 0.2
-num_rows = arr.shape[0]
-split_border = split_level * num_rows
+split_level = 0.2  # podział danych w celu przygotowania częsci do sprawdzenia
+# działania modelu
+num_rows = arr.shape[0]  # ilość rzędów
+split_border = split_level * num_rows  # miejsce podziału danych
 
-arr[:round(split_border), :]
-arr[round(split_border):, :]
+arr[:round(split_border), :]  # wyświetlenie wierszy od początku do split_border
+# i wszystkich kolumn
+arr[round(split_border):, :]  # wyświetlenie wierszy od split border do końca
+# i wszystkich kolumn
+# wartość split board to liczba float, trzeba ją zaokrąglić
 
-np.random.shuffle(arr)
+np.random.shuffle(arr)  # mieszamy wiersze, żeby dane pobierane zawsze z początku
+# zbioru były bardziej losowe
 arr
 
 arr[:round(split_border), :]
 arr[round(split_border):, :]
+
+# piszemy kod, który dzieli dane na uczące i testowe oraz rozdziela je na zbiór
+# feature i zbiór target
 
 data = np.arange(500).reshape(100, 5)
 data
@@ -68,10 +76,12 @@ split_level = 0.2
 num_rows = data.shape[0]
 split_border = split_level * num_rows
 
-X_train = data[round(split_border):, :-1]
-X_test = data[:round(split_border), :-1]
-y_train = data[round(split_border):, -1]
-y_test = data[:round(split_border), -1]
+X_train = data[round(split_border):, :-1]  # 80% danych - wszystkie kolumny oprócz
+# ostatniej
+X_test = data[:round(split_border), :-1]  # 20% danych - wszystkie kolumny oprócz
+# ostatniej
+y_train = data[round(split_border):, -1]  # 80% danych - ostatnia kolumna
+y_test = data[:round(split_border), -1]  # 20% danych - ostatnia kolumna
 
 data.shape
 X_train.shape
@@ -88,3 +98,14 @@ X_train.shape
 X_test.shape
 y_train.shape
 y_test.shape
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    data[:, :-1], data[:, -1], test_size=0.2, shuffle=True)
+
+X_train.shape
+X_test.shape
+y_train.shape
+y_test.shape
+
