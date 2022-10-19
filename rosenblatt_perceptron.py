d1a9bffd80895039e4f3a893d215448f79beed15
Agnieszka.Gabka <agn.gabka@gmail.com>
2022-10-19 19:19:29 +0200

    added description, explained and analzyed functions simulation of Rosenblatt's perceptron

--- rosenblatt_perceptron.py
+++ rosenblatt_perceptron.py
@@ -1,12 +1,19 @@
 import numpy as np
 
 X = np.arange(-25, 25, 1).reshape(10, 5)
+# tablica 10 wierszy i 5 kolumn o wartościach od -25 do 24
 
 ones = np.ones((X.shape[0], 1))
+# tablica, zawierająca tyle iwerszy co X i jedną kolumnę wypełnioną jedynkami
 
 X_1 = np.append(X.copy(), ones, axis=1)
+# zmienna powstała przez dkolejenie do zmiennej X kolumny pochdzącej z ones
 
 w = np.random.rand(X_1.shape[1])
+
+
+# wektor, który ma tyle samo współrzędnych co ilość kolumn w zmiennaj X_1
+# Wartości współrzędnych są wylosowane
 
 
 def predict(x, w):
@@ -15,8 +22,14 @@ def predict(x, w):
     return y_pred
 
 
+# funkcja mnożaca x (jeden wiersz z tablicy X_1) oraz w
+# dla total_stimulation > 0 zwraca 1, w przeciwnym razie -1
+# test dla w i X_1[0,]
+
 print(predict(X_1[0,], w))
 
 for x in X_1:
     y_pred = predict(x, w)
     print(y_pred)
+
+# dla każdego wiersza z X_1 pętla wywołuje funkcję predict
